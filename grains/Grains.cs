d42ebf6ba7e95a03d4d57f774ec8e0332c84872e
Haridas <haridasnykiel@gmail.com>
2019-01-13 17:17:01 +0000

    Completed the grains exercism exercise.

---
+++ grains/Grains.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class Grains {
+    public static ulong Square (int n) {
+
+        if (n <= 0 || n > 64) {
+            throw new ArgumentOutOfRangeException ();
+        }
+
+        ulong numberOfGrains = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == 0)
+                numberOfGrains = 1;
+            else
+                numberOfGrains += numberOfGrains;
+        }
+        return numberOfGrains;
+    }
+
+    public static ulong Total () {
+        ulong total = 0;
+        for (int i = 1; i <= 64; i++)
+        {
+            total += Square(i);
+        }
+        return total;
+    }
+
+}
